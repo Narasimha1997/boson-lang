ee6c9eacb4850a90ceee3773f8af2238dc6e6411
Narasimha1997 <yogaimpression@gmail.com>
2021-06-27 18:39:55 +0530

    nplang-compiler: Added definitions for tokens and keywords

--- nplang-compiler/src/lexer/types.rs
+++ nplang-compiler/src/lexer/types.rs
@@ -13,11 +13,43 @@ pub const KEYWORDS: &'static [&'static str] = &[
 ];
 
 #[allow(dead_code)]
+pub enum KeywordKind  {
+    KInvalid  = 0,
+    KIf       = 1,
+    KElse     = 2,
+    KWhile    = 3,
+    KFor      = 4,
+    KBreak    = 5,
+    LContinue = 6,
+    KConst    = 7,
+    KVar      = 8,
+    KNone     = 9,
+    KFunc     = 10,
+    KReturn   = 11,
+    KTry      = 12,
+    KCatch    = 13,
+    KFinally  = 14,
+    KRethrow  = 15,
+    KThrow    = 16,
+    KAs       = 17,
+    KTrue     = 18,
+    KFalse    = 19,
+    KForEach  = 20,
+    KIn       = 21,
+    KIndexed  = 22,
+    KPure     = 23,
+}
+
+#[allow(dead_code)]
 pub enum TokenKind {
     Invalid (char),
-    I8 (i8),
-    I16 (i16),
-    I32 (i32),
-    I64 (i64),
 
+    Integer (i128),
+    Str (String),
+    Float (f64),
+
+    Identifier (String),
+    Operator (String),
+    Keyword (KeywordKind),
+    Unknown (String)
 }
