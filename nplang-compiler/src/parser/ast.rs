e8a61a02f1ce194eb81b8bfdd17bb15281823a0d
Narasimha1997 <yogaimpression@gmail.com>
2021-07-03 12:55:47 +0530

    nplang-compiler: added more parser types

--- nplang-compiler/src/parser/ast.rs
+++ nplang-compiler/src/parser/ast.rs
@@ -6,14 +6,61 @@ pub struct Node {
     pos: usize          // position is stored for debugging and error reporting purposes.
 };
 
-pub enum StatementKind {
+pub struct FunctionType {
 
+}
+
+pub struct CallType {
+
+}
+
+pub struct IfElseType {
+    condition: Box<ExpressionKind>,
+    main_condition: BlockStatement,
+    alternate_condition: Option<BlockStatement>
+}
+
+pub struct TryCatchType {
+    try_block: BlockStatement,
+    exception_ident: IdentifierType,
+    catch_block: BlockStatement
+}
+
+pub struct IdentifierType {
+    name: String,
+    type: Option<String>
+}
+
+pub struct Let {
+    Identifier (IdentifierKind)
+    Expression (ExpressionKind)
+}
+
+
+pub enum LiteralKind {
+    Int(i64),
+    Float(f64),
+    Str(String),
+    Array(Vec<ExpressionKind>),
+    HashTable<Vec<(ExpressionKind, ExpressionKind)>>
+}
+
+pub enum ExpressionKind {
+
+}
+
+pub enum StatementKind {
+    Empty,
+    Let (LetKind),
+    Return (ExpressionKind),
+    Throw (ExpressionKind),
+    Expression (ExpressionKind)
 };
 
-pub enum BlockStatementKind {
+pub struct BlockStatement {
     Statements(Vec<StatementKind>)
 }
 
-pub enum Program {
+pub struct Program {
     Statements(Vec<StatementKind>)
 }
