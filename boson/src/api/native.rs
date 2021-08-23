f6199cf94c9530ad60d03916c418fba2c83821a8
Narasimha1997 <yogaimpression@gmail.com>
2021-08-23 12:28:26 +0530

    boson: Added native print, println and exec commands

--- boson/src/api/native.rs
+++ boson/src/api/native.rs
@@ -1,6 +1,7 @@
 use crate::types::object;
 
 use std::rc::Rc;
+use std::process::Command;
 
 use object::Object;
 
@@ -17,5 +18,29 @@ pub fn println(st: &String) {
 }
 
 pub fn exec(args: &Vec<Rc<Object>>) -> Result<(i32, Vec<u8>), String> {
-    
+    let mut command = Command::new(args[0].as_ref().describe());
+    for idx in 1..args.len() {
+        command.arg(args[idx].as_ref().describe());
+    }
+
+    let result = command.output();
+    if result.is_err() {
+        return Err(format!("Sub Command Error: {}", result.unwrap_err()));
+    }
+
+    // return the output:
+    let cmd_result = result.unwrap();
+    let output_data = if cmd_result.status.success() {
+        cmd_result.stdout
+    } else {
+        cmd_result.stderr
+    };
+
+    // get the exit status code:
+    let exit_code = match cmd_result.status.code() {
+        Some(code) => code,
+        None => return Err("Failed to get exit code.".to_string()),
+    };
+
+    return Ok((exit_code, output_data));
 }
