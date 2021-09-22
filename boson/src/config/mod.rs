4a236147fb0282a6516406b411db198c7fd0b992
Narasimha1997 <yogaimpression@gmail.com>
2021-09-22 15:04:59 +0530

    boson: Added more options to config

--- boson/src/config/mod.rs
+++ boson/src/config/mod.rs
@@ -1,6 +1,17 @@
 
+// TODO: Move into a configurable format like JSON/TOML later
 
 pub const BYTE_ENDIAN: &str = "little";
 pub const FRAME_STACK_SIZE: usize = 2048;
 pub const DATA_STACK_SIZE: usize = 20480;
 pub const GLOBAL_POOL_SIZE: usize = 65536;
+
+// static-data-stack:
+// This will allocate data-stack as a static array
+// provides a performance gain as vector resize will be avoided.
+pub const USE_STATIC_DATA_STACK: bool = true;
+
+// enable-concurrency
+// Enabl-concurrency features, if disabled
+// multi-threading code will run sequentially
+pub const ENABLE_CONCURRENCY: bool = true;
