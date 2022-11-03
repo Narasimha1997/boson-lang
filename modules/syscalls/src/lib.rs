907dde7e809db1e536e8eef771bd084a0d399e20
Narasimha Prasanna HN <yogaimpression@gmail.com>
2022-11-03 13:00:29 +0530

    Add raw system call interface for x86_64 arch

--- modules/syscalls/src/lib.rs
+++ modules/syscalls/src/lib.rs
@@ -1,6 +1,8 @@
 extern crate boson;
 extern crate syscall_numbers;
 
+mod platform;
+
 use boson::types::{
     dyn_module::DynamicModuleInternalError, dyn_module::DynamicModuleResult, object::Object,
 };
@@ -51,14 +53,87 @@ fn get_syscalls_as_map() -> Rc<Object> {
     Rc::new(Object::HashTable(RefCell::new(hash_map)))
 }
 
+#[inline(always)]
+unsafe fn syscall_raw(no: usize, params: &[Rc<Object>]) -> Result<Rc<Object>, String> {
+    let mut usize_pointers = vec![];
+    usize_pointers.resize(params.len(), 0);
+
+    for param in params.iter() {
+        match param.as_ref() {
+            Object::Str(str) => usize_pointers.push(str.as_ptr() as usize),
+            Object::ByteBuffer(buffer) => {
+                usize_pointers.push(buffer.borrow().data.as_ptr() as usize);
+            }
+            Object::Int(i) => {
+                usize_pointers.push(*i as usize);
+            }
+            _ => {
+                return Err(format!(
+                    "value of type {} cannot be converted into a syscall compatible parameter",
+                    param.get_type()
+                ))
+            }
+        }
+    }
+
+    let syscall_result = match usize_pointers.len() {
+        0 => platform::syscall0(no),
+        1 => platform::syscall1(no, &usize_pointers),
+        2 => platform::syscall2(no, &usize_pointers),
+        3 => platform::syscall3(no, &usize_pointers),
+        4 => platform::syscall4(no, &usize_pointers),
+        5 => platform::syscall5(no, &usize_pointers),
+        6 => platform::syscall6(no, &usize_pointers),
+        _ => {
+            return Err(format!(
+                "max supported system call arguments are 6, but got {}",
+                usize_pointers.len()
+            ))
+        }
+    };
+
+    Ok(Rc::new(Object::Int(syscall_result as i64)))
+}
+
 #[no_mangle]
-pub fn exec(method: String, _params: &Vec<Rc<Object>>) -> DynamicModuleResult {
+pub fn exec(method: String, params: &Vec<Rc<Object>>) -> DynamicModuleResult {
     match method.as_ref() {
         "get_syscalls" => return Ok(get_syscalls_as_map()),
+        "call" => {
+            if params.len() == 0 {
+                return Err(DynamicModuleInternalError {
+                    cause: "no_method".to_string(),
+                    message: format!("call() accepts at least one parameter"),
+                });
+            }
+
+            match params[0].as_ref() {
+                Object::Int(i) => unsafe {
+                    let result = syscall_raw(*i as usize, &params[1..]);
+                    if result.is_ok() {
+                        return Ok(result.unwrap());
+                    } else {
+                        return Err(DynamicModuleInternalError {
+                            cause: "internal_err".to_string(),
+                            message: result.unwrap_err(),
+                        });
+                    }
+                },
+                _ => {
+                    return Err(DynamicModuleInternalError {
+                        cause: "invalid_type".to_string(),
+                        message: format!(
+                            "call() accepts first parameter of type int but got {}",
+                            params[0].get_type()
+                        ),
+                    })
+                }
+            }
+        }
         _ => {
             return Err(DynamicModuleInternalError {
                 cause: "no_method".to_string(),
-                message: format!("method {} not found", method)
+                message: format!("method {} not found", method),
             })
         }
     }
---
+++ modules/syscalls/src/platform/x86_64.rs
@@ -0,0 +1,132 @@
+
+use core::arch::asm;
+
+
+#[inline]
+pub unsafe fn syscall0(n: usize) -> usize {
+    let mut ret: usize;
+    asm!(
+        "syscall",
+        inlateout("rax") n as usize => ret,
+        out("rcx") _,
+        out("r11") _,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+
+#[inline]
+pub unsafe fn syscall1(n: usize, args: &[usize]) -> usize {
+    let mut ret: usize;
+    asm!(
+        "syscall",
+        inlateout("rax") n as usize => ret,
+        in("rdi") args[0],
+        out("rcx") _,
+        out("r11") _,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+#[inline]
+pub unsafe fn syscall2(n: usize, args: &[usize]) -> usize {
+    let mut ret: usize;
+    asm!(
+        "syscall",
+        inlateout("rax") n as usize => ret,
+        in("rdi") args[0],
+        in("rsi") args[1],
+        out("rcx") _,
+        out("r11") _,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+
+#[inline]
+pub unsafe fn syscall3(
+    n: usize,
+    args: &[usize]
+) -> usize {
+    let mut ret: usize;
+    asm!(
+        "syscall",
+        inlateout("rax") n as usize => ret,
+        in("rdi") args[0],
+        in("rsi") args[1],
+        in("rdx") args[2],
+        out("rcx") _,
+        out("r11") _,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+
+#[inline]
+pub unsafe fn syscall4(
+    n: usize,
+    args: &[usize]
+) -> usize {
+    let mut ret: usize;
+    asm!(
+        "syscall",
+        inlateout("rax") n as usize => ret,
+        in("rdi") args[0],
+        in("rsi") args[1],
+        in("rdx") args[2],
+        in("r10") args[3],
+        out("rcx") _,
+        out("r11") _,
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+
+#[inline]
+pub unsafe fn syscall5(
+    n: usize,
+    args: &[usize],
+) -> usize {
+    let mut ret: usize;
+    asm!(
+        "syscall",
+        inlateout("rax") n as usize => ret,
+        in("rdi") args[0],
+        in("rsi") args[1],
+        in("rdx") args[2],
+        in("r10") args[3],
+        in("r8")  args[4],
+        out("rcx") _, 
+        out("r11") _, 
+        options(nostack, preserves_flags)
+    );
+    ret
+}
+
+
+#[inline]
+pub unsafe fn syscall6(
+    n: usize,
+    args: &[usize]
+) -> usize {
+    let mut ret: usize;
+    asm!(
+        "syscall",
+        inlateout("rax") n as usize => ret,
+        in("rdi") args[0],
+        in("rsi") args[1],
+        in("rdx") args[2],
+        in("r10") args[3],
+        in("r8")  args[4],
+        in("r9")  args[5],
+        out("rcx") _, 
+        out("r11") _, 
+        options(nostack, preserves_flags)
+    );
+    ret
+}
